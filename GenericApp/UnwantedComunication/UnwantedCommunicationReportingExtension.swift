199a59dccc45aa670405c91fa6eba1bd956a5728
Rubens <rubens.machion@gmail.com>
2024-10-11 16:07:43 -0300

    no message

--- GenericApp/UnwantedComunication/UnwantedCommunicationReportingExtension.swift
+++ GenericApp/UnwantedComunication/UnwantedCommunicationReportingExtension.swift
@@ -16,37 +16,31 @@ class UnwantedCommunicationReportingExtension: ILClassificationUIExtensionViewCo
     }
 
     override func prepare(for classificationRequest: ILClassificationRequest) {
-        // Configure your views for the classification request
         if let call = classificationRequest as? ILCallClassificationRequest,
            let sender = call.callCommunications.last?.sender {
-            phone = try? phoneNumberUtility.parse(sender)
-            guard let phone = self.phone else {
-                return
-            }
-            let formatted = phoneNumberUtility.format(phone, toType: .international)
-            numberLabel?.text = "Sample Number: \(formatted)"
+            numberLabel?.text = "Number: \(sender.friendlyNumber())"
 
         } else if let message = classificationRequest as? ILMessageClassificationRequest,
                   let sender = message.messageCommunications.last?.sender {
 
-            numberLabel?.text = "Sample Number: \(sender)"
+            numberLabel?.text = "Number: \(sender.friendlyNumber())"
         }
     }
 
     override func classificationResponse(for request:ILClassificationRequest) -> ILClassificationResponse {
 
         var payload: [String: AnyHashable] = [
-            "description": "Spam Generic App"
+            "description": "Suspected SPAM"
         ]
 
         if let call = request as? ILCallClassificationRequest,
            let sender = call.callCommunications.last?.sender {
-            payload["number"] = sender
+            payload["number"] = sender.unfriendlyNumber()
             payload["type"] = "PHONE_CALL"
 
         } else if let message = request as? ILMessageClassificationRequest,
                   let sender = message.messageCommunications.last?.sender {
-            payload["number"] = sender
+            payload["number"] = sender.unfriendlyNumber()
             payload["type"] = "TEXT"
         }
         if payload["number"] != nil {
@@ -58,5 +52,27 @@ class UnwantedCommunicationReportingExtension: ILClassificationUIExtensionViewCo
         } else {
             return .init(action: .none)
         }
+    }
+}
+
+private extension String {
+    func friendlyNumber() -> String {
+        return format(type: .international)
+    }
+
+    func unfriendlyNumber() -> String {
+        return format(type: .e164)
+    }
+
+    private func format(type: PhoneNumberFormat) -> String {
+        let phoneNumberUtility = PhoneNumberUtility()
+
+        guard let phone = try? phoneNumberUtility.parse(self) else {
+            return self
+        }
+
+        let formatted = phoneNumberUtility.format(phone, toType: type)
+
+        return formatted
     }
 }
